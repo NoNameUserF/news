4aa18f8ce55c1c8863c440a16891989dc852a1d6
WonderJack <jackjonson4@mail.ru>
2024-04-02 01:30:54 +0300

    third commit

--- src/components/NewsBanner/NewsBanner.tsx
+++ src/components/NewsBanner/NewsBanner.tsx
@@ -2,18 +2,16 @@ import {FC} from "react";
 import Image from '../ImageBanner/Image.tsx'
 import style from './style.module.css'
 import {formatTimeAgo} from "../../helpers/formatTimeAgo.ts";
-interface IItem {
+interface IItems {
     item : any
 }
 
-export const NewsBanner:FC<IItem> = ({item}) => {
+export const NewsBanner:FC<IItems> = ({item}) => {
     return (
         <div className={style.banner}>
-            <Image img ={item?.image}/>
-            <h2 className={style.title}>{item?.title}</h2>
-            <p className={style.extra}>{formatTimeAgo(item?.published)} by {item?.author}</p>
+            <Image img ={item.image}/>
+            <h2 className={style.title}>{item.title}</h2>
+            <p className={style.extra}>{formatTimeAgo(item.published)} by {item.author}</p>
         </div>
     );
 }
-
-yar
