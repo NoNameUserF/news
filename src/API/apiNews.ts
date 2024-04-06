7450c73357f86628549f277fdb2de2dbb7def724
WonderJack <jackjonson4@mail.ru>
2024-04-07 00:17:00 +0300

    add pagination

--- src/API/apiNews.ts
+++ src/API/apiNews.ts
@@ -2,11 +2,13 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_BASE_NEWS_URL
 const apiKey = import.meta.env.VITE_NEWS_API_KEY
 
-export const getNews =async  ()  => {
+export const getNews =async  (page_number = 1  , page_size = 10)  => {
     try {
-        const response  = await axios.get(`${baseURL}latest-news`, {
+        const response  = await axios.get(`${baseURL}search`, {
             params : {
-                apiKey : apiKey
+                apiKey : apiKey,
+                page_number ,
+                page_size
             }
         })
         console.log(response.data.news)
