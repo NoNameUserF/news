ffb759b6d4ab08550a15ec1af9370ab44ef5fa7a
WonderJack <jackjonson4@mail.ru>
2024-04-07 15:52:03 +0300

    faang

--- src/API/apiNews.ts
+++ src/API/apiNews.ts
@@ -2,13 +2,28 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_BASE_NEWS_URL
 const apiKey = import.meta.env.VITE_NEWS_API_KEY
 
-export const getNews =async  (page_number = 1  , page_size = 10)  => {
+export const getNews =async  ({page_number = 1, page_size = 10 , category}:any )  => {
     try {
         const response  = await axios.get(`${baseURL}search`, {
             params : {
                 apiKey : apiKey,
                 page_number ,
-                page_size
+                page_size,
+                category
+            }
+        })
+        console.log(response.data.news)
+        return  await response.data
+    }catch (error) {
+        console.log(error)
+    }
+}
+export const getCategories =async  ()  => {
+    try {
+        const response  = await axios.get(`${baseURL}available/categories`, {
+            params : {
+                apiKey : apiKey,
+
             }
         })
         console.log(response.data.news)
